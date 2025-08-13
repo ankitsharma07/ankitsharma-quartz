e3e77df04b9172677d800f14c6193f7736f800da
Ankit Sharma <ankit@nezubn.com>
2025-08-13 23:50:38 +0530

    about

--- content/index.md
+++ content/index.md
@@ -2,4 +2,4 @@
 title: An Engineer's Musings
 ---
 
-Hi, I am Ankit aka nezubn on the internet.
+Hi, I am Ankit aka nezubn [ipa nezubn /nɛzʌb/ — no meaning, i made it up] on the internet.
--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
     configuration: {
         pageTitle: "nezubn",
-        pageTitleSuffix: "",
+        pageTitleSuffix: "nezubn",
         enableSPA: true,
         enablePopovers: true,
         analytics: {
